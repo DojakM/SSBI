5d2f4d6301ed9a30d97f53258c9758fccb0eb45c
dominikmolitor <dominik-jakob.molitor@protonmail.com>
2022-05-18 18:35:48 +0200

    Assignment 03

--- Assignment03/Molitor_Moroff_pdb_parser.py
+++ Assignment03/Molitor_Moroff_pdb_parser.py
@@ -1,8 +1,12 @@
 import glob
+import matplotlib.pyplot as plt
 
 def read_residues(line):
-    line = line[19:]
-    return line.split(" ")
+    line = line[20:]
+    line = line.strip()
+    values = line.split(" ")
+
+    return values
 
 def read_helix_entry(line):
     first_entry = line[22:25]
@@ -16,7 +20,7 @@ def read_helix_entry(line):
         print("Incorrect read-in")
         quit()
     length = last_entry-first_entry
-    return (type, length)
+    return type, length, first_entry, last_entry
 
 def read_sheet_entry(line):
     first_entry = line[23:26]
@@ -28,25 +32,95 @@ def read_sheet_entry(line):
         print("Incorrect read-in")
         quit()
     length = last_entry - first_entry
-    return length
+    return length, first_entry, last_entry
+
+def read_atoms(line):
+    if line[18] == "N":
+        return None
+    if line[18] == "O":
+        return None
+
 
 if __name__ == '__main__':
     helices = []
     sheets = []
     residues_list = []
+    amino_acids = {"ALA":0, "ARG":1, "ASN":2, "ASP":3, "CYS":4, "GLU":5, "GLN":6,
+                   "GLY":7, "HIS":8, "ILE":9, "LEU":10, "LYS":11, "MET":12, "PHE":13,
+                   "PRO":14,  "SER":15,  "THR":16, "TRP":17,  "TYR":18,  "VAL":19}
+    aa_list = ["ALA", "ARG", "ASN", "ASP", "CYS", "GLU", "GLN",
+                   "GLY", "HIS", "ILE", "LEU", "LYS", "MET", "PHE",
+                   "PRO",  "SER",  "THR", "TRP",  "TYR",  "VAL"]
+    aa_propensity_1 = [0]*20
+    aa_propensity_2 = [0] * 20
     for path in glob.glob("Supplementary/**"):
+        read_in = False
+        chain = "w"
+        offset = []
+        hel_pla = []
+        she_pla = []
         residues = []
         with open(path, 'r') as file:
             for line in file:
                 if line.startswith("HELIX"):
-                    helices.append(read_helix_entry(line))
+                    type, leng, first, last = read_helix_entry(line)
+                    helices.append((type, leng))
+                    if type == 1:
+                        hel_pla.append((first, last))
                 elif line.startswith("SHEET"):
-                    sheets.append(read_sheet_entry(line))
+                    leng, first, last = read_sheet_entry(line)
+                    sheets.append(leng)
+                    she_pla.append((first, last))
                 elif line.startswith("SEQRES"):
                     residues += read_residues(line)
+                elif line.startswith("ATOM"):
+                    if not read_in:
+                        offset.append(int(line[23:26]))
+                        read_in = True
+                        chain = line[22]
+                    if chain != line[22]:
+                        read_in = False
+                    read_atoms(line)
         residues_list.append(residues)
+        counter = offset[0]
+        run = 0
+        print(len(offset))
+        for i in hel_pla:
+            for j in range(i[0], i[1]):
+                if len(offset)>run+1:
+                    if counter + j >= offset[run+1]:
+                        counter = offset[run+1]
+                        run += 1
+                try:
+                    aa_propensity_1[amino_acids.get(residues[j-counter])] = aa_propensity_1[amino_acids.get(residues[
+                                                                                                               j-counter])]+ 1
+                except:
+                    continue
 
-    with open("output.txt", 'w') as file:
+        for i in she_pla:
+            for j in range(i[0], i[1]):
+                if len(offset) > run + 1:
+                    if counter + j >= offset[run + 1]:
+                        counter = offset[run + 1]
+                        run += 1
+                try:
+                    aa_propensity_2[amino_acids.get(residues[j-counter])] = aa_propensity_2[amino_acids.get(residues[
+                                                                                                               j-counter])]+1
+                except:
+                    continue
+
+        with open("output2.txt", 'w') as file:
+            total1 = sum(aa_propensity_1)
+            total2 = sum(aa_propensity_2)
+            string = "Amino acids\talpha-helix\tbeta-sheet\n"
+
+            for i in range(0, 20):
+                percent1 = aa_propensity_1[i]/total1
+                percent2 = aa_propensity_2[i]/total2
+                string += "{}\t{}\t{}\n".format(aa_list[i], str(percent1), str(percent2))
+            file.write(string)
+
+    with open("output.txt ", 'w') as file:
         sheet_len = sum(sheets)
         helices_len = 0
         alpha = 0
@@ -60,7 +134,6 @@ if __name__ == '__main__':
         total = 0
         for residues in residues_list:
             total += len(residues)
-        str = "Secondary structure\tAbundance\nright-handed alpha helix\t{}\nright-handed 3_10 helices\t{}\nbeta-sheets" \
-              "\t{}\n".format(alpha/total, hel_310/total, sheet_len/total)
 
-        file.write(str)
+        string = "Secondary structure\tAbundance\nright-handed alpha helix\t{}\nright-handed 3_10 helices\t{}\nbeta-sheets\t{}\n".format(alpha/total, hel_310/total, sheet_len/total)
+        file.write(string)
